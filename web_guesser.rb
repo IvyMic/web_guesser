9de39da4079be435a7e7c1512562ee9a961aa502
IvyMic <ibrahim_e@hotmail.co.uk>
2018-01-18 14:38:44 +0000

    background changes based on accuracy of attempt

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,26 +6,50 @@ require "sinatra/reloader"
 
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
+  status = check_guess(guess)
+  background_colour = set_background(status)
+  message = set_message(status)
   #renders the ERB template named index and creates local variable for template
   #named number which has same value as the number variable from this server code.
-  erb :index, :locals => {:number => @@number, :message => message }
+  erb :index, :locals => {:number => @@number, :message => message, :background_colour => background_colour }
 end
 
 def check_guess(guess)
   if guess > @@number
     if (guess - @@number) > 15
-      return "Way too high!"
+      return :too_high
     else
-      return "Too high!"
+      return :high
     end
   elsif guess < @@number
     if (@@number - guess) > 15
-      return "Way too low!"
+      return :too_low
     else
-      return "Too low!"
+      return :low
     end
   elsif @@number == guess
-    return "Congrats!\nThe secret number is #{@@number}"
+    return :correct
   end
+end
+
+def set_background(status)
+  background = {
+    :too_high => "red",
+    :high => "coral",
+    :too_low => "red",
+    :low => "coral",
+    :correct => "green"
+  }
+  background[status]
+end
+
+def set_message(status)
+  message = {
+    :too_high => "Way too high!",
+    :high => "Too high!",
+    :too_low => "Way too low!",
+    :low => "Too low!",
+    :correct => "Congrats!\nThe secret number is #{@@number}"
+  }
+  message[status]
 end
