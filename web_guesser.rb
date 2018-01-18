12353ee6c3f6c1e526f2ebe850e9f6a45553333d
IvyMic <ibrahim_e@hotmail.co.uk>
2018-01-18 15:05:57 +0000

    added cheat mode and guess counter

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,18 +3,30 @@ require "sinatra/reloader"
 
 
 @@number = rand(100)
+@@guesses = 6
 
 get '/' do
+  @@guesses -= 1
   guess = params['guess'].to_i
+  cheat = params['cheat']
   status = check_guess(guess)
   background_colour = set_background(status)
   message = set_message(status)
+  message = message + "\n it's not that serious, here's the secret number: #{@@number}" if cheat == "true"
+  restart if status == :correct || status == :game_over
   #renders the ERB template named index and creates local variable for template
   #named number which has same value as the number variable from this server code.
-  erb :index, :locals => {:number => @@number, :message => message, :background_colour => background_colour }
+  erb :index, :locals => {:number => @@number, :message => message, :background_colour => background_colour, :guesses => @@guesses }
+end
+
+def restart
+  @@number = rand(100)
+  @@guesses = 6
 end
 
 def check_guess(guess)
+  return :start if @@guesses == 5
+  return :game_over if @@guesses == 0
   if guess > @@number
     if (guess - @@number) > 15
       return :too_high
@@ -38,7 +50,8 @@ def set_background(status)
     :high => "coral",
     :too_low => "red",
     :low => "coral",
-    :correct => "green"
+    :correct => "green",
+    :game_over => "blue" #why not
   }
   background[status]
 end
@@ -49,7 +62,9 @@ def set_message(status)
     :high => "Too high!",
     :too_low => "Way too low!",
     :low => "Too low!",
-    :correct => "Congrats!\nThe secret number is #{@@number}"
+    :correct => "Congrats!\nThe secret number is #{@@number}!!!\n Play again: ",
+    :start => "Have fun!",
+    :game_over => "You're out of guesses, unlucky! The secret number is #{@@number}!!!\n Play again: "
   }
   message[status]
 end
